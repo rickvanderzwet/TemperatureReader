d637ec03a4073af7e7cbe2e7f16dac813b75906a
Rick van der Zwet <info@rickvanderzwet.nl>
2017-11-10 12:08:41 +0100

    Temperature reading on the Pi
    Script inspired by http://domoticx.com/raspberry-pi-temperatuur-sensor-ds18b20-uitlezen/

---
+++ readtemp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Read temperature of 1-wire sensors
+#
+import sys
+import time
+import csv
+import glob
+
+while True:
+  csvfile = time.strftime("/home/pi/DHT_DATA_%Y_%m.csv")
+  with open(csvfile, "a") as output:
+    for tfile in glob.glob('/sys/bus/w1/devices/*/w1_slave'):
+      sensor = tfile.split('/')[5]
+      with open(tfile, 'rb') as fh:
+        # $ cat /sys/bus/w1/devices/28-031655c9d5ff/w1_slave 
+        # 51 01 4b 46 7f ff 0c 10 ab : crc=ab YES
+        # 51 01 4b 46 7f ff 0c 10 ab t=21062
+        text = fh.read()
+    
+      # Retreive t=(\d*) value in milli-degrees
+      secondline = text.split("\n")[1]
+      temperaturedata = secondline.split(" ")[9]
+      temp = int(temperaturedata[2:])
+  
+      print "sensor", sensor, "=", float(temp) / 1000, "graden."
+    
+      data = [int(time.time()), sensor, temp]
+      writer = csv.writer(output, delimiter=";", lineterminator='\n')
+      writer.writerow(data)
+
+  time.sleep(5)
+
+
+
